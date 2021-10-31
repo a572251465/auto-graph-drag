d2dc7b3a890bf14c6ed380e72bd5cc5fb5ea5c95
lihaohao <2495041749@qq.com>
2021-10-31 09:41:48 +0800

    feat: first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,25 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  extends: [
+    'airbnb-base',
     'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
+    'prettier'
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'import/extensions': 'off'
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'node_modules/@vue/cli-service/webpack.config.js'
+      }
+    }
   }
 }
